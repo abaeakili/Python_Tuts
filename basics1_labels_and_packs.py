2ebbf1207919523dcd72a5dddaf8f107927f1746
Abae Akili <mohaabae@yahoo.com>
2020-11-15 10:16:01 +0300

    🎐 Creating basics defined window

---
+++ basics1_labels_and_packs.py
@@ -0,0 +1,18 @@
+# 1. Labels and The Pack System
+
+import tkinter
+
+# Define the Window
+root = tkinter.Tk()
+root.title("Label Basics")
+root.iconbitmap("liked.ico")
+root.geometry("400x400")
+root.resizable(0,0)
+root.config(bg="blue")
+
+# Create the widgets
+#name_label_1 = tkinter(root, text='Hello, my name is Abae.')
+#name_label_1.pack()
+
+# Run the main root window
+root.mainloop()
